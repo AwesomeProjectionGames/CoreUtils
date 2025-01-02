55a26c23f25519b0ca8b10a1af5117c12e0fd251
eliott <eliott.jaquier@gmail.com>
2025-01-02 19:10:00 +0100

    Add ToCamelCase string extension

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace AwesomeProjectionCoreUtils.Extensions
 {
@@ -18,6 +19,31 @@ namespace AwesomeProjectionCoreUtils.Extensions
                 case "": return "";
                 default: return input[0].ToString().ToUpper() + input.Substring(1);
             }
+        }
+        
+        /// <summary>
+        /// Converts the input string to camel case (first character lowercase and removes spaces).
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <returns>The input string in camel case with spaces removed</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static string ToCamelCase(this string input)
+        {
+            if (input == null) throw new ArgumentNullException(nameof(input));
+            if (string.IsNullOrWhiteSpace(input)) return "";
+
+            // Split the string by spaces, capitalize each word, and combine them
+            var words = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].FirstCharToUpper();
+            }
+
+            // Join words and ensure the first character is lowercase
+            var camelCase = string.Join("", words);
+            return camelCase.Length > 0 
+                ? camelCase[0].ToString().ToLower(CultureInfo.InvariantCulture) + camelCase.Substring(1) 
+                : camelCase;
         }
     }
 }
