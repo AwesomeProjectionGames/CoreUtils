9306de120351be5aa8d636b1da217935d4f56b2b
eliott <eliott.jaquier@gmail.com>
2025-12-16 23:53:27 +0100

    Add monobehavior extensions to get physics scene easily

---
+++ Extensions/MonoBehaviorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace AwesomeProjectionCoreUtils.Extensions
+{
+    public static class MonoBehaviorExtensions
+    {
+        public static PhysicsScene PhysicsScene(this MonoBehaviour monoBehaviour)
+        {
+            return monoBehaviour.gameObject.scene.GetPhysicsScene();
+        }
+    }
+}
